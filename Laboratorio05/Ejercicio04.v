6fe7913e779f84a09ac3e89a567ab6840c9784ac
Angardo18 <48691615+Angardo18@users.noreply.github.com>
2020-08-15 22:22:19 -0600

    Ejercicico completo

--- Laboratorio05/Ejercicio04.v
+++ Laboratorio05/Ejercicio04.v
@@ -15,24 +15,24 @@ module Mux8_1(input wire D0,D1,D2,D3,D4,D5,D6,D7,S2,S1,S0,output wire out);
 
     Mux4_1 m1(D0,D1,D2,D3,S1,S0,out4_1);
     Mux4_1 m2(D4,D5,D6,D7,S1,S0,out4_2);
-    Mux2_1 m3(out4_1,out4_2,out);
+    Mux2_1 m3(out4_1,out4_2,S2,out);
 
 endmodule
 
 module Tabla1Mux8_1(input wire a,b,c, output wire out);
-    Mux8_1  m1(0,1,1,0,1,0,0,1,a,b,c,out);
+    Mux8_1  m1(1'b0,1'b1,1'b1,1'b0,1'b1,1'b0,1'b0,1'b1,a,b,c,out);
 endmodule
 
 module Tabla1Mux4_1(input wire a,b,c,output wire out);
     wire notC;
     assign notC = ~c;
 
-    Mux4_1 m1(c,notc,notc,c,a,b,out);
+    Mux4_1 m1(c,notC,notC,c,a,b,out);
 endmodule
 
 module Tabla1Mux2_1(input wire a,b,c, output wire out);
     wire xorBC,xnorBC;
-    assign xorBC = b * ~C + ~b * c ;
+    assign xorBC = b * ~c + ~b * c ;
     assign xnorBC = ~xorBC;
 
     Mux2_1 m1(xorBC,xnorBC,a,out);
@@ -40,21 +40,22 @@ endmodule
 
 module Tabla2Mux8_1 (input wire a,b,c, output wire out);
 
-    Mux8_1 m1(1,0,0,0,0,1,1,0,a,b,c,out);
+    Mux8_1 m1(1'b1,1'b0,1'b0,1'b0,1'b0,1'b1,1'b1,1'b0,a,b,c,out);
 
 endmodule
 
 module Tabla2Mux4_1 (input wire a,b,c,output wire out);
     wire notC;
-    Mux4_1  m1(1,0,1,notC,a,b,out);
+    assign notC = ~c;
+    Mux4_1  m1(1'b1,1'b0,1'b1,notC,a,b,out);
 endmodule
 
 module Tabla2Mux2_1 (input wire a,b,c, output wire out);
-    wire nandBC, xorBC;
-    assign nandBC = ~(b*c);
-    assign xorBC = b * ~c + ~b *c ;
+    wire norBC, xorBC;
+    assign norBC = ~(b | c);
+    assign xorBC = b & ~c  |  ~b &c ;
 
-    Mux2_1 m1(nandBC,xorBC,a,out);
+    Mux2_1 m1(norBC,xorBC,a,out);
 endmodule
 
 
